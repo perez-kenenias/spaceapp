5fcd6197e506d844018233125ef5624579b03ba2
Kenenias B. Perez Betanzos <kenenias.perez@e-omnisys.com.mx>
2026-05-01 17:00:50 -0600

    ADD Se agregan funcionalidad para zoom de acercar y alejar para las imagenes.

--- src/components/Busqueda.tsx
+++ src/components/Busqueda.tsx
@@ -1,7 +1,24 @@
+import { useRef } from "react"
+
 const Busqueda = () => {
+    
+const refCajaBusqueda = useRef<HTMLInputElement>(null);
+const refParrafoValor = useRef<HTMLParagraphElement>(null);
+
+const handleBuscar = () => {
+    if (!refParrafoValor.current || !refCajaBusqueda.current) return;
+    refParrafoValor.current.textContent = refCajaBusqueda.current.value;
+}
+
+
+
     return (
         <>
-            <input type='text' className='cajaBusqueda' />
+            <div>
+                <input type='text' ref={refCajaBusqueda} className='cajaBusqueda' onChange={ (e) => setValorABuscar(e.target.value) }/>
+                <button className="botonBuscar" onClick={handleBuscar}>Buscar</button>
+            </div>
+            <p style={ {color: 'white'} } ref={refParrafoValor}></p>
         </>
     )
 }
--- src/components/Imagen.tsx
+++ src/components/Imagen.tsx
@@ -1,3 +1,4 @@
+import { useRef } from "react"
 
 interface ImagenData {
   id: number
@@ -14,22 +15,49 @@ interface ImagenProps {
 }
 
 const Imagen = ({ imagen, expandida = false, imagenSeleccionada, actualizaFavoritas }: ImagenProps) => {
+  const imageRef = useRef<HTMLImageElement>(null);;
+
+  const handleAcercar = () => {
+    if(imageRef.current){
+      imageRef.current.style.transform = 'scale(1.5)';
+    }
+  }
+
+  const handleAlejar = () => {
+    if(imageRef.current){
+      imageRef.current.style.transform = 'scale(1)';
+    }
+  }
+
   return (
     <figure
       className='imagenGaleria'
       style={expandida ? { width: '100%' } : undefined}>
-      <img 
-        onClick={() => imagenSeleccionada(imagen.id)} 
-        src={imagen.archivoImagen} alt="imagen" />
+      <div style={{ position: 'relative', overflow: 'hidden' }}>
+        <img
+          ref={imageRef}
+          onClick={() => imagenSeleccionada(imagen.id)}
+          src={imagen.archivoImagen}
+          alt="imagen"
+          style={{ width: '100%', height: 'auto' }} />
+      </div>
 
-      <figcaption className="tituloImagen">
-        <h3>{imagen.titulo}</h3>
-        <div onClick={() => {
-          actualizaFavoritas(imagen.id)
-        }}>
-          {imagen.favorita
-            ? <img src="img/iconos/favorito-activo.png" />
-            : <img src="img/iconos/favorito.png" />}
+      <figcaption>
+        {
+          expandida && <div>
+            <button className="botonBuscar" onClick={handleAcercar}>Acercar</button>
+            <button className="botonBuscar" onClick={handleAlejar}>Alejar</button>
+          </div>
+        }
+        <div className="tituloImagen">
+          <h3>{imagen.titulo}</h3>
+          <div onClick={() => {
+            actualizaFavoritas(imagen.id)
+          }}>
+            {imagen.favorita
+              ? <img src="img/iconos/favorito-activo.png" />
+              : <img src="img/iconos/favorito.png" />}
+          </div>
         </div>
       </figcaption>
     </figure>
