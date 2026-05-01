244c9a755c434ba7cf6129e8765248bbc6b8b226
Kenenias B. Perez Betanzos <kenenias.perez@e-omnisys.com.mx>
2026-05-01 17:24:12 -0600

    FIX Correcion de estilos para la caja de busqueda.

--- src/components/Busqueda.tsx
+++ src/components/Busqueda.tsx
@@ -1,24 +1,26 @@
 import { useRef } from "react"
 
 const Busqueda = () => {
-    
-const refCajaBusqueda = useRef<HTMLInputElement>(null);
-const refParrafoValor = useRef<HTMLParagraphElement>(null);
 
-const handleBuscar = () => {
-    if (!refParrafoValor.current || !refCajaBusqueda.current) return;
-    refParrafoValor.current.textContent = refCajaBusqueda.current.value;
-}
+    const refCajaBusqueda = useRef<HTMLInputElement>(null);
+    const refParrafoValor = useRef<HTMLParagraphElement>(null);
+
+    const handleBuscar = () => {
+        if (!refParrafoValor.current || !refCajaBusqueda.current) return;
+        refParrafoValor.current.textContent = refCajaBusqueda.current.value;
+    }
 
 
 
     return (
         <>
-            <div>
-                <input type='text' ref={refCajaBusqueda} className='cajaBusqueda' />
-                <button className="botonBuscar" onClick={handleBuscar}>Buscar</button>
-            </div>
-            <p style={ {color: 'white'} } ref={refParrafoValor}></p>
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                <div>
+                    <input type='text' ref={refCajaBusqueda} className='cajaBusqueda' />
+                    <button className="botonBuscar" onClick={handleBuscar}>Buscar</button>
+                </div>
+                <p style={{ color: 'white' }} ref={refParrafoValor}></p>
+            </div >
         </>
     )
 }
